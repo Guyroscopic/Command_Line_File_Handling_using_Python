719af0147cb72f2bad60d553bcafe352858b052e
LaraibHere <laraibarjamand@gmail.com>
2020-11-26 22:44:59 +0500

    Create,Delete,ChDir added

--- OS_Project.py
+++ OS_Project.py
@@ -1,9 +1,17 @@
 from OS_Project_utils import *
+import os
 
 ROOT_PATH  = "C:/OS_Project_root"
-MENU_ITEMS = ["Create", "Delete", "MkDir", "Move", "Exit"]
+MENU_ITEMS = ["Create", "Delete", "MkDir", "ChDir", "Move", "Exit"]
 
 os.system("cls")
+
+#if the root os path does not exist
+if not os.path.isdir(ROOT_PATH):
+        os.chdir("C:/")
+        os.mkdir("C:/OS_Project_root")
+        print("Directory did not exist So created one")
+        
 os.chdir(ROOT_PATH)
 
 
@@ -15,29 +23,36 @@ for i, item in enumerate(MENU_ITEMS):
 
 
 while True:
-	
-	command_full = input("\nEnter a Command: ")
 
-	try:
-		command_func = command_full.split()[0]
-	except IndexError as ie:
-		print("\nERROR: No function entered")
-		continue
+        command_full = input("\nEnter a Command: ")
 
-	if command_func not in MENU_ITEMS:
-		print("\nERROR! No such command please try again")
-	else:
-		
-		if command_func == "MkDir":
-			try:
-				dir_path = command_full.split()[1]
-				mkDir(dir_path)
-				print("'" + ROOT_PATH + "/" + dir_path + "' Directory created")
+        try:
+                command_func = command_full.split()[0]
+        except IndexError as ie:
+                print("\nERROR: No function entered")
+                continue
 
-			except IndexError as ie:
-				print("\nERROR: No Directory name or path specified, usage: 'MkDir <directoryPath>'")
+        if command_func not in MENU_ITEMS:
+                print("\nERROR! No such command please try again")
+        else:
+                if command_func == "Create":
+                        file_path = command_full.split()[1]
+                        create(file_path)
+				
+                elif command_func == "MkDir":
+                        dir_path = command_full.split()[1]
+                        mkDir(dir_path)
 
-			except FileExistsError as fee:
-				print(f"\nERROR: Directory '{dir_path}' already exists, delete the previous one to create new")
+                elif command_func == "Delete":
+                        file_path = command_full.split()[1]
+                        delete(file_path)
+
+                elif command_func == "ChDir":
+                        dir_path = command_full.split()[1]
+                        chDir(dir_path)
+
+                elif command_func == "Exit":
+                        print("\nQuitting")
+                        break
 
 
--- OS_Project_utils.py
+++ OS_Project_utils.py
@@ -1,14 +1,57 @@
 import os
 
-def create():
-	pass
+ROOT_PATH  = "C:/OS_Project_root"
+MENU_ITEMS = ["Create", "Delete", "MkDir", "Move", "Exit"]
 
-def delete():
-	pass
+
+def create(file_path):
+        try:
+                open(file_path, 'x')
+                print("'" + ROOT_PATH + "/" + file_path + "' File created")
+
+        except IndexError as ie:
+                print("\nERROR: No Directory name or path specified, usage: 'Create <filePath>'")
+
+        except FileExistsError as fee:
+                print(f"\nERROR: File '{file_path}' already exists, delete the previous one to create new")
+
+def delete(file_path):
+        try:
+                os.remove(file_path)
+                print("'" + ROOT_PATH + "/" + file_path + "' File deleted")
+
+        except IndexError as ie:
+                print("\nERROR: No Directory name or path specified, usage: 'Delete <filePath>'")
+
+        except FileNotFoundError as ffe:
+                print(f"\nERROR: File '{file_path}' does not exist")
+
 
 def mkDir(dir_path):
+        try:
+                os.mkdir(dir_path)
+                print("'" + ROOT_PATH + "/" + dir_path + "' Directory created")
 
-	os.mkdir(dir_path)
+        except IndexError as ie:
+                print("\nERROR: No Directory name or path specified, usage: 'MkDir <directoryPath>'")
+
+        except FileExistsError as fee:
+                print(f"\nERROR: Directory '{dir_path}' already exists, delete the previous one to create new")
+
+
+        
 
 def  Move():
-	pass
+        pass
+
+
+def chDir(dir_path):
+        try:
+                os.chdir(dir_path)
+                print("'In directory " + dir_path + "'")
+
+        except IndexError as ie:
+                print("\nERROR: No Directory name or path specified, usage: 'ChDir <directoryPath>'")
+
+        except FileNotFoundError as ffe:
+                print(f"\nERROR: Directory '{dir_path}' not found")
